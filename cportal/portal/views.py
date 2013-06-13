166394746baf4281a270e1ddb2ea00f9e6bbb7d9
Ivan Voras <ivoras@gmail.com>
2013-06-14 00:48:16 +0200

    Index page ("portal" app) in the web project root

--- cportal/portal/views.py
+++ cportal/portal/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import Http404
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render
+
+def index(request):
+    data = {}
+    return render(request, 'portal/index.html', data)
+
