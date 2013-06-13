51af6ef54ee551ae0f405a099f902420fb662e70
Ivan Voras <ivoras@gmail.com>
2013-06-14 01:38:02 +0200

    Remote connection test

--- cportal/portal/views.py
+++ cportal/portal/views.py
@@ -7,6 +7,15 @@ from django.shortcuts import render
 import paramiko
 
 def index(request):
-    data = {}
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    client.connect("chefnode4", 22, "puser", "puser22")
+
+    stdin, stdout, stderr = client.exec_command("ls -la /")
+    out = stdout.read()
+    client.close()
+
+    data = { "out" : out }
     return render(request, 'portal/index.html', data)
 
